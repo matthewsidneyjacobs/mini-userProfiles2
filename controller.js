5552ce504c9135f5caaf76ef4937fb28948e7bdc
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-11-16 12:26:12 -0700

    finished mini project

--- controller.js
+++ controller.js
@@ -1,10 +1,34 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
+  // $scope.getUsers = function() {
+  // 	$scope.users = mainService.getUsers();
+  // }
+//replacing old way with a promise
+
+
+  $scope.currentPage = 1;
+
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+    mainService.getUsers($scope.currentPage).then(function(response) {
+      $scope.users = response.data.data;
+      $scope.totalPages = response.data.total_pages;
+    })
   }
 
-
   $scope.getUsers();
+
+  $scope.previous = function() {
+    if($scope.currentPage > 1) {
+      $scope.currentPage--;
+      $scope.getUsers();
+  }
+  }
+
+  $scope.next= function() {
+    if ($scope.currentPage < $scope.totalPages) {
+      $scope.currentPage++
+      $scope.getUsers();
+    }
+  }
 
 });
